af5a05dfd469c3fc63df04f868f1c247b6989a73
marcstreeter <mstreeter@gmail.com>
2016-10-04 12:27:25 -0500

    my darth commit

--- Swift App 2/ViewController.swift
+++ Swift App 2/ViewController.swift
@@ -4,15 +4,33 @@
 //
 //  Created by Marc Streeter on 10/4/16.
 //  Copyright © 2016 Marc Joseph  Marc Alan  streeter. All rights reserved.
-//
+//🌯
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+     
+      tapCount = tapCount + 1
+        
+        if tapCount >= 30{
+        
+       theLabel.text = "you tapped the button twenty times!!"
+        
+        }
+        
+          }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+   
     }
 
     override func didReceiveMemoryWarning() {

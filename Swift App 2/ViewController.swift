60d42c5baf5e5ed763ae4cad6766576a62ec7918
marcstreeter <mstreeter@gmail.com>
2016-10-04 10:38:02 -0500

    Initial Commit

---
+++ Swift App 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App 2
+//
+//  Created by Marc Streeter on 10/4/16.
+//  Copyright © 2016 Marc Joseph  Marc Alan  streeter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
